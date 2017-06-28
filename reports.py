66d140a226714c23b181b671fa6e15a96a2fb43e
somaSzolyak <soma.szolyak@gmail.com>
2017-06-28 12:32:04 +0200

    count_games and decide pass test

--- reports.py
+++ reports.py
@@ -1,2 +1,23 @@
 
-# Report functions
+
+def count_games(file_name):
+    game_counter = 0
+    with open(file_name) as game_names_file:
+        while game_names_file.readline() != "":
+            game_counter += 1
+    return game_counter
+
+
+def decide(file_name, year):
+    data_str = str("not empty")
+    data_list = list()
+    with open(file_name) as games_file:
+        data_str = games_file.readline()
+        while len(data_str) > 1:
+            data_list = data_str.split("\t")
+            data_list[4] = data_list[4].rstrip()
+            if int(data_list[2]) == year:
+                return True
+
+            data_str = games_file.readline()
+    return False
