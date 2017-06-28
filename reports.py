5d86edaf69d0e2b27726c944c34c2df58fa5223e
somaSzolyak <soma.szolyak@gmail.com>
2017-06-28 12:42:38 +0200

    3 pass - get_latest passes too

--- reports.py
+++ reports.py
@@ -18,6 +18,22 @@ def decide(file_name, year):
             data_list[4] = data_list[4].rstrip()
             if int(data_list[2]) == year:
                 return True
-
             data_str = games_file.readline()
     return False
+
+
+def get_latest(file_name):
+    data_str = str("not empty")
+    data_list = list()
+    cur_latest = 0
+    latest_title = str()
+    with open(file_name) as games_file:
+        data_str = games_file.readline()
+        while len(data_str) > 1:
+            data_list = data_str.split("\t")
+            data_list[4] = data_list[4].rstrip()
+            if int(data_list[2]) > cur_latest:
+                cur_latest = int(data_list[2])
+                latest_title = data_list[0]
+            data_str = games_file.readline()
+    return latest_title
